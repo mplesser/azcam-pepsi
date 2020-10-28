27407438c47c89a26700e8d9e11dc74720af0feb
Michael Lesser <mlesser@arizona.edu>
2020-10-28 15:19:30 -0700

    cleanup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "ipython",
+    "azcam",
+    "azcam-ds9",
+    "azcam-focus",
+    "azcam-observe",
+]
+
+setup(
+    name="azcam-bok",
+    version="20.3",
+    description="azcam environment for Bok systems",
+    long_description=long_description,
+    author="Michael Lesser",
+    author_email="mlesser@arizona.edu",
+    keywords="python parameters",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+)
